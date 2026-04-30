4bb42a4f1ba5b7d9e9515bfca6bb942833fb8569
Anthony <anthonytguilherme@hotmail.com>
2026-04-30 19:13:52 -0300

    refactor: reorganizes functions and improves readability 3.0

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_rpg_dice/widgets/history_list.dart';
 import '../services/dice_service.dart';
 import '../models/roll_result.dart';
 import '../widgets/dice_button.dart';
@@ -14,7 +15,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final DiceService _diceService = DiceService();
-
   int _result = 0;
   final List<RollResult> _history = [];
   final List<int> _diceValues = [4, 6, 8, 10, 12, 20];
@@ -37,50 +37,36 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Center(child: Text(widget.title, style: TextStyle(fontWeight: FontWeight.bold),))),
+        title: Center(
+          child: Text(
+            widget.title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
       body: Column(
         children: [
-          const SizedBox(height: 16),
-
+          SizedBox(height: 16),
           Wrap(
             spacing: 16,
             runSpacing: 16,
             alignment: WrapAlignment.center,
             children: _diceValues.map((dice) {
-              return DiceButton(
-                dice: dice,
-                onPressed: () => _rollD(dice),
-              );
+              return DiceButton(dice: dice, onPressed: () => _rollD(dice));
             }).toList(),
           ),
-
-          const SizedBox(height: 30),
-
+          SizedBox(height: 30),
           Column(
             children: [
-              const Text('Result'),
+              Text('Result'),
               Text(
                 '$_result',
-                style: const TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
               ),
             ],
           ),
-
-          const SizedBox(height: 20),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: _history.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_history[index].toString()),
-                );
-              },
-            ),
-          ),
+          SizedBox(height: 20),
+          HistoryList(history: _history),
         ],
       ),
     );
---
+++ lib/widgets/history_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:project_rpg_dice/models/roll_result.dart';
+
+class HistoryList extends StatefulWidget {
+  final List<RollResult> history;
+  const HistoryList({super.key, required this.history});
+
+  @override
+  State<HistoryList> createState() => _HistoryListState();
+}
+
+class _HistoryListState extends State<HistoryList> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: widget.history.length,
+        itemBuilder: (context, index) {
+          return ListTile(title: Text(widget.history[index].toString()));
+        },
+      ),
+    );
+  }
+}
